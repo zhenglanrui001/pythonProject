db5bd09284e16e2483b98bc245eb22aaed9da32d
zhenglanrui001 <l.r.zheng@nefu.edu.cn>
2023-05-05 20:13:10 +0800

    Lab 1

---
+++ 2023实验/Lab1/1.py
@@ -0,0 +1,27 @@
+﻿class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class Linked_List:
+    def __init__(self):
+        self.head = None
+    def add(self, value):
+        node1 = Node(value)
+        node1.next = self.head
+        self.head = node1
+
+    def initlist(self, data_list):
+        for word in data_list:
+            self.add(word)
+
+    def reverse(self):
+        return self.head
+
+    def print_list(self):
+        print("linked_list:")
+        current = self.head
+        alist = []
+        while current:
+            alist.append(current.data)
+            current = current.next
+        print(alist)
